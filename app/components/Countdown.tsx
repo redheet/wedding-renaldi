a2aceceb69c4733a817065fe9ae4a3099d9c4d49
redheet <naufalzidane32@gmail.com>
2024-07-22 16:10:33 +0700

    rev final 1.6 fix error countdown

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Countdown from "react-countdown";
 import { RevealWrapper } from "next-reveal";
 
-function CountdownComp() {
+const CountdownComp: React.FC = () => {
   function renderer({
     days,
     hours,
